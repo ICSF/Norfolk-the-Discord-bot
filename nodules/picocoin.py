5b11cbe97b4a368eca5ad6c4808ab0f72fc7d88c
Jakub Dranczewski <domdlapsa@gmail.com>
2021-02-14 23:27:03 +0000

    Better coin logic, give/take/check functions

--- nodules/picocoin.py
+++ nodules/picocoin.py
@@ -11,6 +11,9 @@ password = "password"
 
 
 async def get_total():
+    """
+    Get the total amount of money raised in the fundraiser.
+    """
     async with ClientSession() as session:
         async with session.get(url + 'fundraising/pages/{}'.format(fundraiser), headers={"Accept": "application/json"},
                                auth=BasicAuth(username, password)) as r:
@@ -20,6 +23,9 @@ async def get_total():
 
 
 async def get_donations():
+    """
+    Get the number and list of donations.
+    """
     async with ClientSession() as session:
         async with session.get(url + 'fundraising/pages/{}/donations'.format(fundraiser), headers={"Accept": "application/json"},
                                auth=BasicAuth(username, password)) as r:
@@ -34,22 +40,32 @@ class Picocoin:
         self.dbconn.execute('''CREATE TABLE IF NOT EXISTS "picocoin" (
                                "id" INTEGER UNIQUE,
                                "user_id" INTEGER UNIQUE,
-                               "total" NUMERIC DEFAULT 0,
+                               "total" NUMERIC DEFAULT 1,
                                PRIMARY KEY("id" AUTOINCREMENT)
                                );''')
 
-    def ensure(self, user_id):
+    def check(self, user_id):
+        # Check the balance of a user, creating their entry id none
         self.dbconn.execute("INSERT OR IGNORE INTO picocoin (user_id) VALUES (?)", (user_id,))
         self.dbconn.commit()
+        return float(self.dbconn.execute("SELECT total FROM picocoin WHERE user_id=?", (user_id,)).fetchone()["total"])
 
     def give(self, user_id, amount):
-        self.ensure(user_id)
+        # Give user Picocoin
+        total = self.check(user_id)
         self.dbconn.execute("UPDATE picocoin SET total = total + ? WHERE user_id = ?", (amount, user_id))
         self.dbconn.commit()
+        return total + amount
 
     def take(self, user_id, amount):
-        self.take(user_id, -amount)
-
+        # Take Picocoin from the user, can't go below 0
+        total = self.check(user_id)
+        if total - amount >= 0:
+            self.dbconn.execute("UPDATE picocoin SET total = total + ? WHERE user_id = ?", (-amount, user_id))
+            self.dbconn.commit()
+            return total - amount
+        else:
+            raise ValueError("The final amount is smaller than 0.")
 
 
 class Nodule(CoreNodule):
@@ -75,12 +91,50 @@ class Nodule(CoreNodule):
         if message.content.startswith('!give') and message.author.guild_permissions.administrator:
             _, user, amount, = message.content.split(" ")
             try:
-                self.client.picocoin.give(user[3:-1], float(amount))
+                total = self.client.picocoin.give(user[3:-1], float(amount))
+                embed = Embed(description="<:picocoin:810623980222021652> {} was given **{:.2f} Picocoin**. They now have **{:.2f} Picocoin**.".format(
+                    user, float(amount), total
+                  ),
+                  colour=Colour.green())
+                await message.channel.send(embed=embed)
             except ValueError:
-                await message.channel.send("Please provide a valid amount.")
+                total = self.client.picocoin.check(user[3:-1])
+                embed = Embed(description="<:picocoin:810623980222021652> Please provide a valid amount. {} has {} Picocoin.".format(
+                    user, total
+                  ),
+                  colour=Colour.red())
+                await message.channel.send(embed=embed)
 
-    async def on_raw_reaction_add(self, payload):
-        pass
+        if message.content.startswith('!take') and message.author.guild_permissions.administrator:
+            _, user, amount, = message.content.split(" ")
+            try:
+                total = self.client.picocoin.take(user[3:-1], float(amount))
+                embed = Embed(description="<:picocoin:810623980222021652> {} has paid **{:.2f} Picocoin**. They now have **{:.2f} Picocoin**.".format(
+                    user, float(amount), total
+                ),
+                    colour=Colour.green())
+                await message.channel.send(embed=embed)
+            except ValueError:
+                total = self.client.picocoin.check(user[3:-1])
+                embed = Embed(
+                    description="<:picocoin:810623980222021652> Please provide a valid amount. {} has {} Picocoin.".format(
+                        user, total
+                    ),
+                    colour=Colour.red())
+                await message.channel.send(embed=embed)
+
+        if message.content.startswith('!picocoin'):
+            total = self.client.picocoin.check(message.author.id)
+            embed = Embed(description="<:picocoin:810623980222021652> You ({}) have **{:.2f} Picocoin**.".format(message.author.mention, total),
+                colour=Colour.green())
+            await message.channel.send(embed=embed)
+
+        if message.content.startswith('!balance') and message.author.guild_permissions.administrator:
+            _, user,  = message.content.split(" ")
+            total = self.client.picocoin.check(user[3:-1])
+            embed = Embed(description="<:picocoin:810623980222021652> {} has **{:.2f} Picocoin**.".format(user, total),
+                          colour=Colour.green())
+            await message.channel.send(embed=embed)
 
     @loop(seconds=30)
     async def get_donations(self, message=None):
@@ -101,7 +155,10 @@ class Nodule(CoreNodule):
         number, donations = await get_donations()
         embed = Embed(title="Charity donations", description="Donate now at https://icsf.org.uk/donate",
                       colour=Colour.from_rgb(202, 138, 0))
-        embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/808080178965381171/810481381971853312/picocoin.gif")
+        if message:
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/808080178965381171/810481381971853312/picocoin.gif")
+        else:
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/808080178965381171/810496226322808892/0008.png")
         embed.add_field(name="Total donated", value="£{:.2f}".format(total))
         embed.add_field(name="Donations", value=number)
         if message:
